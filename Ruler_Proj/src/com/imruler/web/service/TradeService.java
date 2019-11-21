4aa8b02b98055048dee013804515431b8e2fe942
suyoung <suyoungpk@naver.com>
2019-11-21 17:18:01 +0900

    파일 폴더 삭제

--- Ruler_Proj/src/com/imruler/web/service/TradeService.java
+++
@@ -1,5 +0,0 @@
-package com.imruler.web.service;
-
-public interface TradeService {
-
-}
--- Ruler_Proj/src/com/imruler/web/service/trade/RulerTradeService.java
+++
@@ -1,7 +0,0 @@
-package com.imruler.web.service.trade;
-
-import com.imruler.web.service.TradeService;
-
-public class RulerTradeService implements TradeService {
-
-}
