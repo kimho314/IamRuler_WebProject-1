2936e04c8668ac0f623046629db94437325e8865
78958869 <a@a.a>
2019-12-03 10:02:06 +0900

    회원가입 조건 세분화 1

--- Ruler_Proj/src/com/imruler/web/controller/member/SignUpController.java
+++ Ruler_Proj/src/com/imruler/web/controller/member/SignUpController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.internal.StringUtil;
+
 import com.imruler.web.entity.Member;
 import com.imruler.web.service.MemberService;
 import com.imruler.web.service.member.RulerMemberService;
@@ -47,17 +49,25 @@ public class SignUpController extends HttpServlet {
 			String gender = req.getParameter("성별");
 			int age = Integer.parseInt(req.getParameter("연령대"));
 			String bodyshape = req.getParameter("체형");
-			int isDuplicated = memberService.isDuplicatedId(userId, userPwd, userPwdRequest, phone, email);
+			int isDuplicated = memberService.isDuplicatedId(userId, userPwd, userPwdRequest, phone, email); // 에러확인 1차
+			
+			if( StringUtil.isNumeric( req.getParameter("전화번호") ) == false ) { // 전화번호가 숫자만 들어가있는지 확인
+				isDuplicated = 3;
+			}
 
-			if (isDuplicated != 0) {
-				resp.sendRedirect("sign-up?error=" + isDuplicated);
+			if (req.getParameter("전화번호").length() != 11) { // 전화번호가 11자가 아닐경우 에러
+				isDuplicated = 3;
+			}
+			
+			if (isDuplicated != 0) { // 에러 확인값이 0이 아닐경우 에러
+				resp.sendRedirect("sign-up?error=" + isDuplicated); // 에러 발생시 해당하는 코드값으로 이동
 			} else {
 				memberService.insertMember(
-						new Member(userId, userPwd, phone, email, height, weidth, gender, age, bodyshape));
+						new Member(userId, userPwd, phone, email, height, weidth, gender, age, bodyshape)); // 회원가입시 정보입력
 				resp.sendRedirect("/sign/login");
 			}
 		} else {
-			resp.sendRedirect("sign-up?error=0");
+			resp.sendRedirect("sign-up?error=0"); // 알수없는 에러
 		}
 	}
 }
